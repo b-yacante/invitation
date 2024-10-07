38430d61bfaa04585666a273319434b9d9c8d046
Braian Yacante <byacante@i-rent.net>
2024-10-07 12:06:27 -0300

    view page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import { MdArrowOutward } from "react-icons/md";
 import Image from "next/image";
+import { Updock, Bodoni_Moda } from "next/font/google";
+
+const updock = Updock({ weight: ["400"], subsets: ["latin"] });
+const bodoni = Bodoni_Moda({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen">
+      {/* background */}
+      <div className="fixed min-h-screen min-w-full opacity-90">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="invitation.svg"
+          alt="invitation"
+          layout="fill"
+          objectFit="contain"
+          quality={100}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </div>
+      <main className="flex flex-col items-center justify-center">
+        <div className={`${updock.className} text-3xl`}>
+          <div>Te invito</div>
+        </div>
+        <div className="flex justify-center items-center max-h-24 space-x-2">
+          <p className={`${updock.className} text-xl translate-y-3`}>a</p>
+          <div className={`text-[150px] ${updock.className}`}>15</div>
+          <p className={`${updock.className} text-xl translate-y-3`}>mis</p>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div
+          className={`flex justify-center items-center text-base ${bodoni.className} mt-12`}
+        >
+          {/* Dia */}
+          <div className="flex justify-center items-center max-h-10 border-dashed border-y-2 border-[#406086] px-1">
+            <p>SÁBADO</p>
+          </div>
+          <div className="grid place-items-center gap-y-2">
+            <div>DICIEMBRE</div>
+            <div className="h-14 w-14 text-3xl bg-slate-200 rounded-full grid place-items-center">
+              14
+            </div>
+            {/* Ubicacion */}
+            <Link
+              href="https://www.google.com/maps?q=-31.6434939,-68.5900726&z=17&hl=es"
+              target="_blank"
+              className={`flex justify-center items-center translate-x-1`}
+            >
+              <p className="text-base">SALÓN</p>
+              <MdArrowOutward className="text-slate-400 text-xs" />
+            </Link>
+          </div>
+          {/* Horario */}
+          <div className="flex justify-center items-center max-h-10 border-dashed border-y-2 border-[#406086] px-1">
+            9:30 PM
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
